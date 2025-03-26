b88d5a9379e25d6eebfb2a480ca6032784e68b3b
Heather Selim <heatherselim25@gmail.com>
2025-03-26 20:47:44 +0200

    edited the code

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,15 +1,9 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework.authtoken.models import Token
-from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
-from .models import User
-from accounts.models import CustomUser
-from .models import CustomUser
+from accounts.models import CustomUser  # Use this instead of generic User
 
-#CustomUser = get_user_model()
-User = get_user_model()
-#.objects.create_user
+User = get_user_model()  # This should now refer to CustomUser
 
 class UserDetailSerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,14 +11,27 @@ class UserDetailSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email']
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
 
-class UserLoginSerializer(serializers.Serializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
+        )
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
-    password = serializers.CharField()
+    password = serializers.CharField(write_only=True)
 
     def validate(self, data):
         username = data.get('username')
@@ -37,86 +44,10 @@ class UserLoginSerializer(serializers.Serializer):
             data['user'] = user
         else:
             raise serializers.ValidationError("Must include 'username' and 'password'.")
-
         return data
 
 
-class CustomUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'password', 'email', 'bio', 'profile_picture', 'followers')
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password'],
-            bio=validated_data.get('bio', ''),
-            profile_picture=validated_data.get('profile_picture', None)
-        )
-        return user
-
-class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'password', 'email')  # Include necessary fields
-
-    def create(self, validated_data):
-        user = User(**validated_data)
-        user.set_password(validated_data['password'])  # Hash the password
-        
-        
-        user.save()
-        return user
-
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField(required=True)
-    password = serializers.CharField(required=True, write_only=True)
-
-    def validate(self, data):
-        serializer = LoginSerializer(data=request.data)
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email']
-    
 class UserFollowSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username')    
-
-class UserRegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = User(**validated_data)
-        user.set_password(validated_data['password'])  # Hash the password
-        user.save()
-        return user
-    
-class RegistrationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
-
-    def create(self, validated_data):
-        user = CustomUser.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
-        return user
-
-    print("Debug:", CustomUser._meta)
-    print("Model in Serializer:", User)
-    print("CustomUser model:", CustomUser)  # Add this in serializers.py
-
-     
+        fields = ['id', 'username']
